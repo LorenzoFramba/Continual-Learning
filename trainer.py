5428aed4036a9b9cf9d5e9c89b520669c4dd6e66
Lorenzo Framba <rappasta@mbp-di-lorenzo.fbkeduroam.it>
2019-11-13 12:09:17 +0100

    preso cosi com'e' dalla repo

--- trainer.py
+++ trainer.py
@@ -1,15 +1,11 @@
 import torch
 from torch import optim
 import torch.nn as nn
-import torchvision as tv
-from torchvision import transforms
-
 from models import fcn, unet, pspnet, dfn
 from datasets.voc import to_rgb
 import torch.backends.cudnn as cudnn
 from torch.optim.lr_scheduler import LambdaLR
-import matplotlib.pyplot as plt
-import numpy as np 
+
 
 import time
 from datetime import timedelta
@@ -26,11 +22,6 @@ except ImportError:
 
 cudnn.benchmark = True # For fast speed
 
-def imshow(self, img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
 class Trainer:
     def __init__(self, train_data_loader, val_data_loader, config):     #gli passiamo i due dataset e la stringa di comandi
         self.cfg = config
@@ -40,12 +31,6 @@ class Trainer:
 
         self.build_model()      #crea il modello 
 
-    def imshow(self, img):
-        print("entrato")
-        img = img / 2 + 0.5     # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
     def denorm(self, x):
         out = (x + 1) / 2
         return out.clamp_(0, 1)    #ritorna il valore, 0 se minore di 0 e 1 se maggiore di 1. lo " NORMALIZZA " ( penso)
@@ -84,7 +69,7 @@ class Trainer:
                                        w_image_size=self.cfg.w_image_size)      #settiamo la dimensione W
         self.optim = optim.Adam(self.model.parameters(),                        #usiamo adam per ottimizzazione stocastica come OPTIM, passangogli i parametri
                                 lr=self.cfg.lr,                                 #settiamo il learning rate
-                                betas=[self.cfg.beta1, self.cfg.beta2])     #le due Beta, cioe' la probabilita' di accettare l'ipotesi quando e' falsa  (coefficients used for computing running averages of gradient and its square )
+                                betas=[self.cfg.beta1, self.cfg.beta2])     #le due Beta, cioe' la probabilita' di accettare l'ipotesi quando e' falsa
         # Poly learning rate policy
         lr_lambda = lambda n_iter: (1 - n_iter/self.cfg.n_iters)**self.cfg.lr_exp       #ATTENZIONE: learning rate LAMBDA penso
         self.scheduler = LambdaLR(self.optim, lr_lambda=lr_lambda)
@@ -102,23 +87,11 @@ class Trainer:
 
     def train_val(self):
 
-
-        
-
         # Compute epoch's step size
         iters_per_epoch = len(self.train_data_loader.dataset) // self.cfg.train_batch_size      #divisione
         if len(self.train_data_loader.dataset) % self.cfg.train_batch_size != 0:
             iters_per_epoch += 1
         epoch = 1
-        iters_per_epoch=5
-
-        print('batch size {}\t'
-                      'dataset size : [{}]\t'
-                      'epoch : [{}]\t'
-                      'datasetTime // batch : {}\t'.format(
-                          self.cfg.train_batch_size,iters_per_epoch,len(self.train_data_loader.dataset), 
-                      len(self.train_data_loader.dataset) // self.cfg.train_batch_size
-                      ))
 
         torch.cuda.synchronize()  # parallel mode
         self.model.train()
@@ -142,7 +115,7 @@ class Trainer:
 
             self.reset_grad()           #resetta il gradiente
             loss.backward()         #va indietro del loss
-            self.optim.step()   #update i parametri dopo l'ottimizzazione
+            self.optim.step()   #ottimizza tramite ADAM. DOBBIAMO CAPIRE STO ADAM
             print('Done')#
 
             #output_label = torch.argmax(_output, dim=1)#
@@ -168,36 +141,28 @@ class Trainer:
         val_start_time = time.time()                                     #prendiamo il tempo
         data_iter = iter(self.val_data_loader)                           #iteriamo su data_loader
         max_iter = len(self.val_data_loader)                            #prendiamo la dimensione
-        for n_iter in range(max_iter):
-            self.scheduler.step()
-            try:
-                input, target = next(data_iter)     #passa il prossimo elemento dall'iteratore, input=immagine e target=mask
-            except:
-                data_iter = iter(self.train_data_loader)
-                input, target = next(data_iter)
-            #n_iter =  0
-            #input, target = next(data_iter)                         #andiamo avanti nell'iteratore
+        # for n_iter in range(max_iter): #FIXME
+        n_iter =  0
+        input, target = next(data_iter)                         #andiamo avanti nell'iteratore
 
-            input_var = input.clone().to(self.device)               #copiamo in device (GPU) input_var
-            target_var = target.to(self.device)                 #copiamo la maschera in device
+        input_var = input.clone().to(self.device)               #copiamo in device (GPU) input_var
+        target_var = target.to(self.device)                 #copiamo la maschera in device
 
-            output = self.model(input_var)                      #l'output diventa l'immagine (PENSO)
-            _output = output.clone()                   
-            output = output.view(output.size(0), output.size(1), -1)
-            target_var = target_var.view(target_var.size(0), -1)
-            loss = self.c_loss(output, target_var)                  #come prima
+        output = self.model(input_var)                      #l'output diventa l'immagine (PENSO)
+        _output = output.clone()                                    #copiamo l'output
+        # output = output.view(output.size(0), output.size(1), -1)
+        # target_var = target_var.view(target_var.size(0), -1)
+        loss = self.c_loss(output, target_var)                  #come prima
 
-            output_label = torch.argmax(_output, dim=1)     #aggiunto
-            #imshow(tv.utils.make_grid(input))      #aggiunto
-            if (n_iter + 1) % self.cfg.log_step == 0:               #controlla questo LOG_STEP
-                seconds = time.time() - val_start_time                  #tempo trascorso per validare
-                elapsed = str(timedelta(seconds=seconds))
-                print('### Validation\t'
-                      'Iteration : [{iter}/{iters}]\t'
-                    'Time : {time:}\t'
-                    'Loss : {loss:.4f}\t'.format(
-                    iter=n_iter+1, iters=max_iter,
-                    time=elapsed, loss=loss.item()))
+        if (n_iter + 1) % self.cfg.log_step == 0:               #controlla questo LOG_STEP
+            seconds = time.time() - val_start_time                  #tempo trascorso per validare
+            elapsed = str(timedelta(seconds=seconds))
+            print('### Validation\t'
+                  'Iteration : [{iter}/{iters}]\t'
+                  'Time : {time:}\t'
+                  'Loss : {loss:.4f}\t'.format(
+                  iter=n_iter+1, iters=max_iter,
+                  time=elapsed, loss=loss.item()))
 
         if USE_NSML:                            #non lo usiamo
             ori_pic = self.denorm(input_var[0:4])
