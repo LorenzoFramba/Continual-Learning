57bf03f0e21e158e4283b36a28700e2c7cc95042
LorenzoFramba <lorenzo.framba-1@studenti.unitn.it>
2020-02-13 11:45:31 +0100

    added loss function

---
+++ loss.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch.nn as nn
+import torch
+
+
+
+
+def sigmLoss(output, target):
+    loss = nn.BCELoss(reduce=False)
+    loss1 = nn.CrossEntropyLoss()
+
+    l1 = loss(output, target)
+    l2 = loss1(output, target)
+    print(l1,l2)
